a1482c4df26bfe4c404a40ec6e34415acf550769
Gagancreates <gaganp000999@gmail.com>
2026-04-20 05:17:17 +0000

    Phase 4: document tree snapshot creation in commit_create

--- commit.c
+++ commit.c
@@ -253,3 +253,4 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
 /* head_read follows symbolic ref to get parent commit hash */
 /* head_update atomically swings branch pointer to new commit */
 /* commit stores unix timestamp from time(NULL) */
+/* commit_create builds tree snapshot from staged index */
